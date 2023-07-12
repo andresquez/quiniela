1e7f5f66a944a2a33786c97c1c14ac6d260b8988
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2023-07-12 10:03:01 -0600

    feat: Player model migration

--- quiniela/app/models/player.rb
+++ quiniela/app/models/player.rb
@@ -1,2 +1,16 @@
 class Player < ApplicationRecord
+
+    
+    validates ${:username}, presence: true, uniqueness: true
+    validates ${:password}, presence: true
+
+    has_one :role
+    has_many :predictions
+
+    after_create :update_leaderboard
+
+    def update_leaderboard
+        Leaderboard.create(player_id: self.id, points: 0)
+    end
+
 end
