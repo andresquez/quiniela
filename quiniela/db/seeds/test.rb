5760c8fc0c380bf66b8bb006a02738c94d8aa46e
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2023-07-13 14:12:00 -0600

    feat: added seeds for different environments

--- quiniela/db/seeds/test.rb
+++ quiniela/db/seeds/test.rb
@@ -2,32 +2,58 @@
 
 # create teams
 20.times do
-    Team.create(name: Faker::Team.name)
-end
-
-# create player
-5.times do
-    Player.create(username: Faker::Internet.username, password: Faker::Internet.password, role_id: 1)
+    Team.create(
+        name: Faker::Team.name
+    )
 end
 
 # create roles
-Role.create(role: "player")
-Role.create(role: "admin")
+Role.find_or_create_by(role: "player")
+Role.find_or_create_by(role: "admin")
+
+# create player
+5.times do
+    Player.create(
+        username: Faker::Internet.username, 
+        password: Faker::Internet.password, 
+        role_id: 1
+    )
+end
+
 
 # create matches
 20.times do
-    Match.create(date_played: Faker::Date.between(from: '2020-05-01', to: '2021-05-01'), team1_id: rand(1..20), team2_id: rand(1..20), goals1:  )
+    Match.create(
+        date_played: Faker::Date.between(from: '2020-05-01', to: '2021-05-01'),
+        team1_id: rand(1..20),
+        team2_id: rand(1..20), 
+        goals1: rand(1..5), 
+        goals2: rand(1..5)
+    )
 end
 
 # create predictions
 20.times do
-    Prediction.create(player_id: rand(1..5), match_id: rand(1..20), goals1: rand(0..5), goals2: rand(0..5))
+    Prediction.create(
+        player_id: rand(1..5), 
+        match_id: rand(1..20), 
+        goals1: rand(0..5), 
+        goals2: rand(0..5),
+        points_awarded: rand(0..3)
+    )
 end
 
 # create leaderboards
 5.times do
-    Leaderboard.create(player_id: rand(1..5), points: rand(0..20))
+    Leaderboard.create(
+        points: rand(0..20),
+        player_id: rand(1..5)
+    )
 end
 
 # create admin
-Player.create(username: "admin", password: "admin", role_id: 2)
+Player.create(
+    username: "admin", 
+    password: "admin", 
+    role_id: 2
+)
