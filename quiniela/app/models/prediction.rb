727b22ba07f8331f4e0224fdd665c4a2a50c1337
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2023-07-21 12:27:46 -0600

    feat: add new points awarded logic
    if the winner is guessed, +1

--- quiniela/app/models/prediction.rb
+++ quiniela/app/models/prediction.rb
@@ -17,21 +17,32 @@ class Prediction < ApplicationRecord
     # update awarded points based on the match result
     def self.check_awarded_points
         Prediction.all.each do |prediction|
-            # reset awarded points
-            prediction.update(points_awarded: 0)
-            # if match result is the same as prediction
-            if prediction.goals1 == prediction.match.goals1 && prediction.goals2 == prediction.match.goals2
-                prediction.update(points_awarded: 3)
-                # if match team 1 or team 2 goals are the same as prediction
-            elsif prediction.goals1 = prediction.match.goals1 || prediction.goals2 == prediction.match.goals2
-                prediction.update(points_awarded: 1)
-                # if match team 1 or team 2 goals difference is the same as prediction
-            elsif prediction.goals1 - prediction.goals2 == prediction.match.goals1 - prediction.match.goals2
-                prediction.update(points_awarded: 1)
-                # if match result doesnt match any of the point awarding criteria
-            else
+            if prediction.match.goals1 == nil || prediction.match.goals2 == nil
                 prediction.update(points_awarded: 0)
             end
+            # reset awarded points
+            prediction.update(points_awarded: 0)
+                # if match result is the same as prediction
+                if prediction.goals1 == prediction.match.goals1 && prediction.goals2 == prediction.match.goals2
+                    prediction.update(points_awarded: 3)
+                    # if match team 1 or team 2 goals are the same as prediction
+                elsif prediction.goals1 = prediction.match.goals1 || prediction.goals2 == prediction.match.goals2
+                    prediction.update(points_awarded: 1)
+                    # if winner is the same as prediction + 1
+                    if prediction.match.goals1 > prediction.match.goals2 && prediction.goals1 > prediction.goals2 || prediction.match.goals1 < prediction.match.goals2 && prediction.goals1 < prediction.goals2
+                        prediction.update(points_awarded: 2)
+                    end
+                    # if match team 1 or team 2 goals difference is the same as prediction
+                elsif prediction.match.goals1 != nil && prediction.match.goals2 != nil && prediction.goals1 - prediction.goals2 == prediction.match.goals1 - prediction.match.goals2
+                    prediction.update(points_awarded: 1)
+                    # if winner is the same as prediction + 1
+                    if prediction.match.goals1 > prediction.match.goals2 && prediction.goals1 > prediction.goals2 || prediction.match.goals1 < prediction.match.goals2 && prediction.goals1 < prediction.goals2
+                        prediction.update(points_awarded: 2)
+                    end
+                    # if match result doesnt match any of the point awarding criteria
+                else
+                    prediction.update(points_awarded: 0)
+                end
         end
     end
 end
